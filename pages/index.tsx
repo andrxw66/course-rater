d4c4e3ffe5248825617d9361edce508563997a3a
andrxw66 <andreyshihalev099@gmail.com>
2022-12-25 19:38:48 +0300

    Rating component done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
-import { Button, Htag, Paragraph, Tag } from '../components'
+import { useState } from 'react'
+import { Button, Htag, Paragraph, Rating, Tag } from '../components'
 
 export default function Home() {
+
+	const [rating, setRating] = useState<number>(4)
+
 	return (
 		<div>
 			<Htag tag='h1'>Courses</Htag>
@@ -8,6 +12,7 @@ export default function Home() {
 			<Button appearance='ghost' arrow='right'>Button</Button>
 			<Paragraph size='l'>dsadasdas</Paragraph>
 			<Tag size='m' color='primary'>asdasdas</Tag>
+			<Rating rating={rating} setRating={setRating} isEditable></Rating>
 		</div>
 	)
 }

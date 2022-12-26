2870a31db91a8b5ed495bb36ba7ed389c0cc21ca
andrxw66 <andreyshihalev099@gmail.com>
2022-12-26 14:12:49 +0300

    layout and footer styles done

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { FooterProps } from './Footer.props'
+import styles from './Footer.module.css'
+import cn from 'classnames'
+import { format } from 'date-fns'
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer {...props} className={cn(className, styles.footer)}>
+			<div>
+				MyTop © 2022 - {format(new Date(), 'yyyy')} All rights reserved.
+			</div>
+			<a href="#" target='_blank'>User agreement</a>
+			<a href="#" target='_blank'>Privacy policy</a>
+		</footer>
 	)
 }
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,19 +3,18 @@ import { Footer } from './Footer/Footer'
 import { Header } from './Header/Header'
 import { SideBar } from './SideBar/SideBar'
 import { LayoutProps } from './Layout.props'
+import styles from './Layout.module.css'
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
-		<>
-			<Header />
-			<div>
-				<SideBar />
-				<div>
-					{children}
-				</div>
-			</div>
-			<Footer />
-		</>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<SideBar className={styles.sidebar} />
+			<main className={styles.body}>
+				{children}
+			</main>
+			<Footer className={styles.footer} />
+		</div>
 	)
 }
 
